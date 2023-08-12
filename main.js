bb08ba2ddb8db2b1cd2dc3d3df6cd482be321e14
kodama <cshd23008@g.nihon-u.ac.jp>
2023-08-13 00:58:34 +0900

    僅かだが進展

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
 const taskValue = document.getElementsByClassName("task_value")[0];
 const taskSubmit = document.getElementsByClassName("task_submit")[0];
-let taskList = [];
-taskList = document.getElementsByClassName("task_list")[0];
-let counter = 0;
+const taskList = document.getElementsByClassName("task_list")[0];
+const tasksArray = []; // タスクを格納する配列
 
-//タスクを生成
+// タスクを生成
 const createTask = (task) => {
-  taskList.push(task);
-  console.log(taskList);
+  console.log(task);
+  tasksArray.push(task); // タスクを配列に追加
 };
 
 // 追加ボタンを作成
 const addTasks = (task) => {
-  // 入力したタスクを追加・表示
-  const listItem = document.createElement("li");
+  if (task !== "") {
+    const listItem = document.createElement("li");
+    listItem.textContent = task;
 
-  // listItem.setAttribute("id", "item" + counter);
-  // counter++;
-  const showItem = taskList.appendChild(listItem);
-  showItem.innerHTML = task;
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    listItem.appendChild(deleteButton);
 
-  // タスクに削除ボタンを付与
-  const deleteButton = document.createElement("button");
-  deleteButton.innerHTML = "Delete";
-  listItem.appendChild(deleteButton);
+    deleteButton.addEventListener("click", (evt) => {
+      evt.preventDefault();
+      deleteTasks(deleteButton);
+    });
 
-  // 削除ボタンをクリックし、イベントを発動（タスクが削除）
-  deleteButton.addEventListener("click", (evt) => {
-    evt.preventDefault();
-    deleteTasks(deleteButton);
-  });
+    taskList.appendChild(listItem);
+  }
 };
 
 // 削除ボタンにタスクを消す機能を付与
@@ -38,14 +34,15 @@ const deleteTasks = (deleteButton) => {
   taskList.removeChild(chosenTask);
 };
 
-// 追加ボタンをクリックし、イベントを発動（タスクが追加）
 taskSubmit.addEventListener("click", (evt) => {
-  if (taskValue.value !== "") {
-    evt.preventDefault();
-    const task = taskValue.value;
-    createTask(task);
-    addTasks(task);
-    taskValue.value = "";
-    console.log(taskList);
-  }
+  evt.preventDefault();
+  const task = taskValue.value;
+  createTask(task);
+  addTasks(task);
+  taskValue.value = "";
+});
+
+// 例: ページロード時に既存タスクを配列から表示
+tasksArray.forEach((task) => {
+  addTasks(task);
 });
